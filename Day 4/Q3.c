fd7e1dbdd629662e51415fc0b1fc51be1eeed136
Puneet Sharma <Punneett01@gmail.com>
2026-06-07 20:41:41 +0530

    Day 4 assignment is uploaded

---
+++ Day 4/Q3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, a = 0, b = 1, c, i;
+
+    printf("Enter position: ");
+    scanf("%d", &n);
+
+    if(n == 1) {
+        printf("Nth term = %d", a);
+    }
+    else if(n == 2) {
+        printf("Nth term = %d", b);
+    }
+    else {
+
+        for(i = 3; i <= n; i++) {
+
+            c = a + b;
+
+            a = b;
+            b = c;
+        }
+
+        printf("Nth term = %d", c);
+    }
+
+    return 0;
+}
