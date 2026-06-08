c58e7ba967c4188969a28e834bb574ba92dcd572
Puneet Sharma <Punneett01@gmail.com>
2026-06-08 21:15:10 +0530

    Day 6 assignment is uploaded

---
+++ Day 6/Q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    int binary, decimal = 0;
+    int base = 1, rem;
+
+    printf("Enter binary number: ");
+    scanf("%d", &binary);
+
+    while(binary > 0) {
+
+        rem = binary % 10;
+
+        decimal = decimal + rem * base;
+
+        base = base * 2;
+
+        binary = binary / 10;
+    }
+
+    printf("Decimal = %d", decimal);
+
+    return 0;
+}
