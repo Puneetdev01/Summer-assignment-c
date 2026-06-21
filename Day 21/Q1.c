490d0ac25f519ec9759c724f1f6a533d40a174d4
Puneet Sharma <Punneett01@gmail.com>
2026-06-21 21:33:36 +0530

    Day 21 assignment is uploaded

---
+++ Day 21/Q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    printf("Enter string: ");
+    gets(str);
+
+    while(str[count] != '\0')
+        count++;
+
+    printf("Length = %d", count);
+
+    return 0;
+}
